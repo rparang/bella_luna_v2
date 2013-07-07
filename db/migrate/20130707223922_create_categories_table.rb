1a643435e04013fca952e4d6bdb89652c739d3e6
rparang <reza@vitrue.com>
2013-07-07 15:45:02 -0700

    blah

---
+++ db/migrate/20130707223922_create_categories_table.rb
@@ -0,0 +1,10 @@
+class CreateCategoriesTable < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.integer :position
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130707223312) do
+ActiveRecord::Schema.define(:version => 20130707223922) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "position"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "menus", :force => true do |t|
     t.string   "name"
